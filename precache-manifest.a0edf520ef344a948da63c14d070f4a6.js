760f36eddb6c76820f7e33d59122d0dd54b9107b
lekshmi <lekshmiak92@gmail.com>
2019-05-17 12:20:50 +0530

    Updates

---
+++ precache-manifest.a0edf520ef344a948da63c14d070f4a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c7f30f662669ea8a561364b64438ca",
+    "url": "/QTrivia/index.html"
+  },
+  {
+    "revision": "09ee616a59623703bf53",
+    "url": "/QTrivia/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ee0c583220033c493dfb",
+    "url": "/QTrivia/static/css/main.bb9b9dbd.chunk.css"
+  },
+  {
+    "revision": "09ee616a59623703bf53",
+    "url": "/QTrivia/static/js/2.179945e6.chunk.js"
+  },
+  {
+    "revision": "ee0c583220033c493dfb",
+    "url": "/QTrivia/static/js/main.5a990574.chunk.js"
+  },
+  {
+    "revision": "031a74289bd44a8a7066",
+    "url": "/QTrivia/static/js/runtime~main.a0acde7f.js"
+  }
+]);
