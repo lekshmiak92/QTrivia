3d37e96647ba926053f572e90037e2cac5ee12a8
lekshmi <lekshmiak92@gmail.com>
2019-07-16 16:57:08 +0530

    removed clloud functions

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,3 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const fetch = require("node-fetch");
 admin.initializeApp();
-
-exports.newGame = functions.database
-  .ref("/rooms/{gameid}")
-  .onCreate((snapshot, context) => {
-    console.log("msg from cloud");
-    snapshot.ref.update({ msg: "cloud fn are fine" });
-    return snapshot.ref.update({ option: "returnvalue" });
-  });
